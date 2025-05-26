e221f438c7508b21da852a44fea16eb9c47e559e
olaniyi <arokoyueb11@gmail.com>
2025-05-26 16:41:43 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Navbar from './globalcomponents/Navbar'
+import Home from './hompage/Home'
+
+function App() {
+
+  return (
+    <div>
+      <Navbar />
+      <Home />
+ </div>
+  )
+}
+
+export default App
